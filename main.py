80ea5580860ef1f0112b04a483af3809c3fe43ae
Mathias Nielsen <sonderskovmathias@gmail.com>
2019-04-10 22:07:59 +0200

    simplified code. WIP save

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from datetime import datetime
 import os
 from tkinter import *
 from tkinter import ttk
+import sys
 
-# Folder setups
-folder = r"C:/Users/MANI/py"
-folder = "/Users/msn/Dropbox/py/Git/OpenKeynote"
-filename = "MYKEYNOTEFILE.TXT"
-fpath = folder + "/" + filename
+#filepaths
+fpath = "/Users/msn/Dropbox/py/Git/OpenKeynote/MYKEYNOTEFILE.TXT"
+folder = "/".join(fpath.split("/")[:-1])
+filename = fpath.split("/")[-1]
+bkfolder = folder + "/KNOTE_backups"
 
-bkfolder = "/KNOTE_backups/"
-
+#overrules premade filepath with cmd prompt input.
+args = sys.argv
+if len(sys.argv) > 1:
+    fpath = sys.argv[-1]
 
 def fixUI():
     """
@@ -23,40 +26,40 @@ def fixUI():
     h = int(b[1])
     root.geometry('%dx%d' % (w+1, h+1))
 
-    ##
-
-
-def createbackup():
+def createbackup(folder, filename, bkfolder):
+    """
+    Backups your file into a backupfolder
+    """
     mytime = datetime.now().strftime("%Y%m%d_%H%M%S")
     try:
         os.mkdir(bkfolder)
     except OSError:
+        #folder already exists
         pass
     try:
-        copyfile(fpath, folder + bkfolder + filename + "_" + mytime)
+        filefirstname = ".".join(filename.split(".")[:-1])
+        targetfile = bkfolder + "/" + filefirstname + "_" + mytime + ".txt"
+        copyfile(folder + "/" + filename, targetfile)
     except FileNotFoundError as e:
-        print("Can't create backup folder!! (Error code: {} )".format(e))
-
+        print("Can't create backup!! (Error code: {} )".format(e))
 
 lengths = []
-rawdata = []
 previeweditem = ""
 editeditem = ""
-with open(fpath, "rb") as f:
-    chars = f.read()
-    for i in chars.split(b"\r"):
-        number = int(len(i)/2)
-        lengths.append(number)
-        rawdata.append(number)
-        rawdata.append(i)
-
+def getlengths(fpath):
+    with open(fpath, "rb") as f:
+        chars = f.read()
+        for i in chars.split(b"\r"):
+            number = int(len(i)/2)
+            lengths.append(number)
+    return lengths
 
 def changeselection(self):
     """
     what happens when changing selection in the treeview..
     """
     global previeweditem
-    e1.config(state=NORMAL)
+
     itemname = l1.focus()
     previeweditem = l1.focus()
     parentname = [i["parent"] for i in itemlist if i["name"] == itemname][0]
@@ -66,14 +69,9 @@ def changeselection(self):
             itemname, parentname))
     else:
         tx1.config(text="previewing: {}".format(itemname))
-    # #tx1.config(text="editing: " + items[index].split("\t")[0])
+
     e1.delete("1.0", END)
     e1.insert(END, itemlist[index]["content"])
-    e1.config(state=DISABLED)
-    # e2.delete("1.0", END)
-    # e2.insert(END, str(itemsbytes[index]))
-    #
-    # root.update()
 
 
 def edititem(self):
@@ -96,43 +94,46 @@ def edititem(self):
     else:
         tx2.config(text="Editing: {}".format(itemname))
 
-
 def saveitem(self):
     """
     saves text from gui back into main dictionary
     and redraws tree, and selects where we were.
     1) resave Dictionary
-    2) redraw UI
-    3) select active item
+    2) update "edit field"
     """
     global itemlist
     global editeditem
-    # update Dictionary
+    global previeweditem
+
     newcontent = e2.get("1.0", END)
-    for i,k in enumerate(itemlist):
+    for i,k in enumerate(itemlist):    # update Dictionary
         if k["name"] == editeditem:
             itemlist[i]["content"] = newcontent
+    if previeweditem == editeditem:
+        e1.delete("1.0", END)
+        e1.insert(END, newcontent)
 
-    # update e1 and e2
-    e1.config(state=NORMAL)
-    e1.delete("1.0", END)
-    e1.insert(END, newcontent)
-    e1.config(state=DISABLED)
-
-
+#WIP
+def savefile(self):
+    """
+    Saves file! WIP
+    """
+    with open(folder + "/" + "KEYTESTout.txt", mode="wb") as f:
+        for s in [1,2,3]:
+            x = str(s)
+            #print(x, file=f)
+            print("test\n")
+            print(b"\r".decode("utf-8"), file=f)
 
 # SETUP GUI
 root = Tk()
 
-
-sf1 = Text(root, height=1, width=17)
-sf1.grid(row=0, column=0)
-sb1 = Button(root, text="X", width=3)
+sf1 = Text(root, height=1, width=17).grid(row=0, column=0)
+sb1 = Button(root, text="SAVEFILE", width=3)
 sb1.grid(row=0, column=1)
+sb1.bind("<ButtonRelease-1>", savefile)
 sb2 = Button(root, text="X", width=3)
 sb2.grid(row=0, column=2)
-#
-
 
 # Setup treebar and scroll
 l1 = ttk.Treeview(root, height=10)
@@ -144,45 +145,24 @@ l1.grid(row=1, column=0, rowspan=9, columnspan=3, sticky=N+S+E)
 yscroll.grid(row=1, column=0, rowspan=9, columnspan=3, sticky=N+S+E)
 l1.bind("<ButtonRelease-1>", changeselection)
 
-
-# Vertical bar
-vb0 = Button(root, text="0")
-vb0.grid(row=0, column=3)
-vb1 = Button(root, text="1")
-vb1.grid(row=1, column=3)
-vb2 = Button(root, text="2")
-vb2.grid(row=2, column=3)
-vb3 = Button(root, text="3")
-vb3.grid(row=3, column=3)
-vb4 = Button(root, text="4")
-vb4.grid(row=4, column=3)
-vb5 = Button(root, text="5")
-vb5.grid(row=5, column=3)
-vb6 = Button(root, text="6")
-vb6.grid(row=6, column=3)
-vb7 = Button(root, text="7")
-vb7.grid(row=7, column=3)
-vb8 = Button(root, text="8")
-vb8.grid(row=8, column=3)
-vb9 = Button(root, text="9")
-vb9.grid(row=9, column=3)
+vbs = [] # Vertical bar
+for a in range(10):
+    vbs.append(Button(root, text=str(a)))
+    vbs[a].grid(row=int(a), column=3)
 
 # label
 tx1 = Label(root, text="Preview", width=50)
 tx1.grid(row=0, column=4, columnspan=3)
-
 tx2 = Label(root, text="Editing", width=50)
 tx2.grid(row=5, column=4)
 
-# e1 = Entry(root, textvariable=entryString, width=75)
-e1 = Text(root, height=14, width=84)
-e1.config(state=NORMAL, padx=10, pady=10)
+e1 = Text(root, height=14, width=84, fg="#555", font=("Courier", 15),
+    padx=10, pady=10)
 e1.grid(row=1, column=4, columnspan=4, rowspan=4)
 
-e2 = Text(root, height=14, width=84)
-e2.config(state=NORMAL)
+e2 = Text(root, height=14, width=84, font=("Courier", 15), borderwidth=3,
+    relief="solid")
 e2.grid(row=6, column=4, columnspan=4, rowspan=4)
-
 
 vb1 = Button(root, text="Edit", width=20)
 vb1.grid(row=5, column=5)
@@ -191,44 +171,38 @@ vb2 = Button(root, text="Save", width=20)
 vb2.grid(row=5, column=6)
 vb2.bind("<ButtonRelease-1>", saveitem)
 
-
 itemlist = []
-createbackup()
+createbackup(folder, filename, bkfolder)
+lengths = getlengths(fpath)
 
+def readfile(fpath):
+    with open(fpath, "r", encoding="utf-16") as f:
+        chars = f.read()
+        count = 0
+        for i, length in enumerate(lengths[:-1]):
+            fromnum = count
+            tonum = count+lengths[i]
+            chunk = str(chars[fromnum:tonum])
+            count += lengths[i]
+            try:
+                name = str(chunk[0:].split("\t")[0])
+            except:
+                name = ""
+            try:
+                content = str(chunk[0:].split("\t")[1])
+            except:
+                content = ""
+            try:
+                parent = chunk[1:].strip().split("\t")[2]
+            except:
+                parent = ""
+            itemlist.append({"name": name, "content": content, "parent": parent})
+    return itemlist
 
-with open(fpath, "r", encoding="utf-16") as f:
-    chunks = []
-    chars = f.read()
-    count = 0
-    for i, length in enumerate(lengths[:-1]):
-        fromnum = count
-        tonum = count+lengths[i]
-        chunk = str(chars[fromnum:tonum])
-        count += lengths[i]
-        try:
-            name = str(chunk[0:].split("\t")[0])
-            # print(name)
-        except:
-            name = ""
-        try:
-            content = str(chunk[0:].split("\t")[1])
-            # for text in content.split("\n"):
-            #     print(" -  {}".format(text))
-        except:
-            content = ""
-        try:
-            parent = chunk[1:].strip().split("\t")[2]
-        except:
-            parent = ""
-        # print(" - ( parent: {} )".format(parent))
-        # print(" ")
-        itemlist.append({"name": name, "content": content, "parent": parent})
-        chunks.append(chunk)
-
-templist = itemlist[:]
-
-
+itemlist = readfile(fpath)
+templist = itemlist[:] #local copy
 uniquenames = set()
+
 while len(templist) > 0:
     for i, item in enumerate(templist):
         parent = item["parent"]
@@ -241,7 +215,6 @@ while len(templist) > 0:
             del templist[i]
             uniquenames.add(item["name"])
 
-print(itemlist)
 # root.geometry("1000x400+120+120")
 root.update()
 root.after(0, fixUI)
