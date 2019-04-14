9ca9342a07b7ff3cf3c54f663bb652f2c0fba4ec
Mathias Sønderskov Nielsen <sonderskovmathias@gmail.com>
2019-04-14 18:37:32 +0200

    ui stuff

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from tkinter import *
 from tkinter import ttk
 import sys
 
+# sharp fonts in high res (https://stackoverflow.com/questions/41315873/
+# attempting-to-resolve-blurred-tkinter-text-scaling-on-windows-10-high-dpi-disp)
+if os.name == "nt":
+    from ctypes import windll, pointer, wintypes
+    try:
+        windll.shcore.SetProcessDpiAwareness(1)
+    except Exception:
+        pass  # this will fail on Windows Server and maybe early Windows
+
+
 # filepaths
 fpath = "/Users/msn/Dropbox/py/Git/OpenKeynote/MYKEYNOTEFILE.TXT"
 fpath = r"C:\Users\MANI\py\MYKEYNOTEFILE.TXT"
@@ -133,7 +143,7 @@ def savefile(self):
     with open(folder + "/" + "KEYTESTout.txt", mode="wb") as f:
         for s in [1,2,3]:
             x = str(s)
-            #print(x, file=f)
+            # print(x, file=f)
             string = "test\n"
             b = bytes(string, 'utf-8')
             print(b, file=f)
@@ -152,48 +162,75 @@ def savefile(self):
 # SETUP GUI
 root = Tk()
 
-sf1 = Text(root, height=1, width=17)
+pw = PanedWindow(root, orient=HORIZONTAL)
+
+pw.pack(fill=BOTH, expand=1)
+
+pane_left = Frame(root)
+pw.add(pane_left)
+#pane_left.grid(row=0, column=0, sticky=E+W+N+S)
+#pane_left.rowconfigure(1, weight=1)
+#pane_left.columnconfigure(1, weight=1)
+
+
+pane_right = Frame(root)
+#pane_right.grid(row=0, column=2, sticky=E+W+N+S)
+pw.add(pane_right)
+
+frame_left = Frame(pane_left)
+frame_left.pack(fill=BOTH, expand=1)
+frame_center = Frame(pane_right)
+frame_center.grid(row=0, column=0, sticky=E+W+N+S)
+frame_right = Frame(pane_right)
+frame_right.grid(row=0, column=1, sticky=E+W+N+S)
+
+sf1 = Text(frame_left, height=1, width=17)
 sf1.grid(row=0, column=0)
-sb1 = Button(root, text="SAVEFILE", width=15)
-sb1.grid(row=0, column=0)
+sb1 = Button(frame_left, text="SAVEFILE", width=15)
+sb1.grid(row=0, column=1)
 sb1.bind("<ButtonRelease-1>", savefile)
-sb2 = Button(root, text="X", width=3)
+sb2 = Button(frame_left, text="X", width=3)
 sb2.grid(row=0, column=2)
 
 # Setup treebar and scroll
-l1 = ttk.Treeview(root, height=10)
-yscroll = Scrollbar(root, orient=VERTICAL)
+l1 = ttk.Treeview(frame_left)
+yscroll = Scrollbar(frame_left, orient=VERTICAL)
 yscroll.config(width=20)
 l1['yscrollcommand'] = yscroll.set
 yscroll['command'] = l1.yview
-l1.grid(row=1, column=0, rowspan=9, columnspan=3, sticky=N+S+E)
-yscroll.grid(row=1, column=0, rowspan=9, columnspan=3, sticky=N+S+E)
+l1.grid(row=1, column=0, columnspan=3,  padx=(
+    40), pady=(10), sticky=N+S+E+W)
+yscroll.grid(row=1, column=0, columnspan=3, sticky=N+S+E)
 l1.bind("<ButtonRelease-1>", changeselection)
+
+frame_left.rowconfigure(1, weight=1)
+frame_left.columnconfigure(1, weight=1)
 
 vbs = []  # Vertical bar
 for a in range(10):
-    vbs.append(Button(root, text=str(a)))
-    vbs[a].grid(row=int(a)+1, column=3)
+    vbs.append(Button(frame_center, text=str(a)))
+    vbs[a].pack(fill=BOTH)
+    # vbs[a].grid(row=int(a)+1)
 
 # label
-tx1 = Label(root, text="Preview", width=50)
-tx1.grid(row=0, column=4, columnspan=3)
-tx2 = Label(root, text="Editing", width=50)
-tx2.grid(row=5, column=4)
+tx1 = Label(frame_right, text="Preview", width=50)
+tx1.grid(row=0, column=0, columnspan=3)
+tx2 = Label(frame_right, text="Editing", width=50)
+tx2.grid(row=2, column=0)
 
-e1 = Text(root, height=14, width=84, fg="#555", font=("Courier", 15),
+e1 = Text(frame_right, fg="#555", font=("Courier", 15),
           padx=10, pady=10)
-e1.grid(row=1, column=4, columnspan=4, rowspan=4)
+e1.grid(row=1, column=0, columnspan=3)
 
-e2 = Text(root, height=14, width=84, font=("Courier", 15), borderwidth=3,
-          relief="solid")
-e2.grid(row=6, column=4, columnspan=4, rowspan=4)
+e2 = Text(frame_right, font=("Courier", 15), borderwidth=3,
+          relief="solid", padx=10, pady=10)
+e2.grid(row=3, column=0, columnspan=3)
 
-vb1 = Button(root, text="Edit", width=20)
-vb1.grid(row=5, column=5)
+vb1 = Button(frame_right, text="Edit", width=20)
+vb1.grid(row=2, column=1)
 vb1.bind("<ButtonRelease-1>", edititem)
-vb2 = Button(root, text="Save", width=20)
-vb2.grid(row=5, column=6)
+vb2 = Button(frame_right, text="Save", width=20)
+vb2.grid(row=2, column=2)
 vb2.bind("<ButtonRelease-1>", saveitem)
 
 itemlist = []
@@ -243,7 +280,23 @@ while len(templist) > 0:
             del templist[i]
             uniquenames.add(item["name"])
 
-# root.geometry("1000x400+120+120")
+
+def resizeui(self):
+    print("newsize")
+
+
+root.columnconfigure(2, weight=1)
+#root.rowconfigure(1, weight=1)
+
+
+root.bind("<Configure>", resizeui)
+width = 1000
+height = 500
+root.winfo_width()
+root.winfo_height()
+x = (root.winfo_screenwidth() // 2) - (width // 2)
+y = (root.winfo_screenheight() // 2) - (height // 2)
+root.geometry("{}x{}+{}+{}".format(width, height, x, y))
 root.update()
 root.after(0, fixUI)
 root.mainloop()
